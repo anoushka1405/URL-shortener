0b576bfa13bf69692483834d6cb4eb8cacb3f3d3
anoushka1405 <anoushka142006@gmail.com>
2026-08-03 12:38:22 +0530

    feat: improve short codes with offset-based Base62 encoding

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 BASE_URL = os.getenv("BASE_URL")
 
-REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+SHORT_CODE_OFFSET = int(os.getenv("SHORT_CODE_OFFSET", 100000))
--- services/base62.py
+++ services/base62.py
@@ -1,11 +1,13 @@
+from config import SHORT_CODE_OFFSET
+
 import string
 
 BASE62_ALPHABET = string.digits + string.ascii_lowercase + string.ascii_uppercase
 
 
 def encode_base62(num: int) -> str:
-    if num == 0:
-        return BASE62_ALPHABET[0]
+
+    num += SHORT_CODE_OFFSET
 
     digits = []
 
